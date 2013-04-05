ca074dc3f37317a89ba1da836af5bbe33fe95c1a
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-04-05 14:59:39 -0400

    Adds agustinf user

---
+++ modules/people/manifests/agustinf.pp
@@ -0,0 +1,14 @@
+class people::agustinf {
+  include googledrive
+
+  $home     = "/Users/${::luser}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+
+  #repository { $dotfiles:
+  #  source  => 'agustin/dotfiles',
+  #  require => File[$my]
+  #}
+
+  include projects::all
+}
