e9ac6cd0e69d363c8b440407dba196a9d7674a5b
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-06-10 16:24:36 -0400

    Install negroku on all ruby versions used in platanus

--- modules/platanus/manifests/environment.pp
+++ modules/platanus/manifests/environment.pp
@@ -40,10 +40,39 @@ class platanus::environment{
   # App deployment
   include heroku
 
+  ruby::gem { 'negroku for 2.0.0-p0':
+    gem     => 'negroku',
+    ruby    => '2.0.0-p0',
+    version => '>=0.0.3'
+  }
+
+  ruby::gem { 'negroku for 2.0.0-p195':
+    gem     => 'negroku',
+    ruby    => '2.0.0-p195',
+    version => '>=0.0.3'
+  }
+
   ruby::gem { 'negroku for 2.0.0':
     gem     => 'negroku',
     ruby    => '2.0.0',
     version => '>=0.0.3'
   }
 
+  ruby::gem { 'negroku for 1.9.3-p125':
+    gem     => 'negroku',
+    ruby    => '1.9.3-p125',
+    version => '>=0.0.3'
+  }
+
+  ruby::gem { 'negroku for 1.9.3-p392':
+    gem     => 'negroku',
+    ruby    => '1.9.3-p392',
+    version => '>=0.0.3'
+  }
+
+  ruby::gem { 'negroku for 1.9.3':
+    gem     => 'negroku',
+    ruby    => '1.9.3',
+    version => '>=0.0.3'
+  }
 }
