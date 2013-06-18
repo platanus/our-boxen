8592cb44e68f8a1483c3630e82f1e212533122af
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-06-18 15:47:19 -0400

    Update negroku to 1.0.0 with multistage

--- modules/platanus/manifests/environment.pp
+++ modules/platanus/manifests/environment.pp
@@ -41,24 +41,24 @@ class platanus::environment{
   ruby::gem { 'negroku for 2.0.0-p0':
     gem     => 'negroku',
     ruby    => '2.0.0-p0',
-    version => '~> 0.0.11'
+    version => '~> 1.0.0'
   }
 
   ruby::gem { 'negroku for 2.0.0-p195':
     gem     => 'negroku',
     ruby    => '2.0.0-p195',
-    version => '~> 0.0.11'
+    version => '~> 1.0.0'
   }
 
   ruby::gem { 'negroku for 1.9.3-p125':
     gem     => 'negroku',
     ruby    => '1.9.3-p125',
-    version => '~> 0.0.11'
+    version => '~> 1.0.0'
   }
 
   ruby::gem { 'negroku for 1.9.3-p392':
     gem     => 'negroku',
     ruby    => '1.9.3-p392',
-    version => '~> 0.0.11'
+    version => '~> 1.0.0'
   }
 }
