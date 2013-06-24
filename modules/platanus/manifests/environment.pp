9ed1f8d758212a8a0cc940811c620fa2a8a4312c
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-06-24 17:01:15 -0400

    Updates negroku
    Also create a class for negroku installations

--- modules/platanus/manifests/environment.pp
+++ modules/platanus/manifests/environment.pp
@@ -37,28 +37,6 @@ class platanus::environment{
 
   # App deployment
   include heroku
+  include negroku
 
-  ruby::gem { 'negroku for 2.0.0-p0':
-    gem     => 'negroku',
-    ruby    => '2.0.0-p0',
-    version => '~> 1.1.0'
-  }
-
-  ruby::gem { 'negroku for 2.0.0-p195':
-    gem     => 'negroku',
-    ruby    => '2.0.0-p195',
-    version => '~> 1.1.0'
-  }
-
-  ruby::gem { 'negroku for 1.9.3-p125':
-    gem     => 'negroku',
-    ruby    => '1.9.3-p125',
-    version => '~> 1.1.0'
-  }
-
-  ruby::gem { 'negroku for 1.9.3-p392':
-    gem     => 'negroku',
-    ruby    => '1.9.3-p392',
-    version => '~> 1.1.0'
-  }
 }
