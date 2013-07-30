b00f346750eb239e57959268e6c963d7a03054b7
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-07-30 17:42:13 -0400

    Add ant dependencies for quehambre deployment

--- modules/projects/manifests/quehambre-web.pp
+++ modules/projects/manifests/quehambre-web.pp
@@ -2,6 +2,26 @@ class projects::quehambre-web {
 
   package { 'optipng': }
 
+  # Add ant dependencies for publish via ssh
+  $ant_lib_path = "/Users/${::boxen_user}/.ant/lib"
+
+  exec { "create ant/lib":
+    command => "mkdir -p ${$ant_lib_path}",
+    creates => "${$ant_lib_path}",
+  }
+
+  exec { "install jsch":
+    command => "curl -s -o ${$ant_lib_path}/jsch-0.1.50.jar http://ufpr.dl.sourceforge.net/project/jsch/jsch.jar/0.1.50/jsch-0.1.50.jar",
+    creates => "${$ant_lib_path}/jsch-0.1.50.jar",
+    onlyif => "test -d ${$ant_lib_path}",
+  }
+
+  exec { "install ant-jsch":
+    command => "curl -s -o ${$ant_lib_path}/ant-jsch-1.8.2.jar http://repo1.maven.org/maven2/org/apache/ant/ant-jsch/1.8.2/ant-jsch-1.8.2.jar",
+    creates => "${$ant_lib_path}/ant-jsch-1.8.2.jar",
+    onlyif => "test -d ${$ant_lib_path}",
+  }
+
   php::project { 'quehambre-web':
     dir           => "${boxen::config::srcdir}/quehambre/web",
     source        => 'git@github.com:platanus-repos/quehambre.git',
