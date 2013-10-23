ea69a81061775f75c23ae65be9a02d9cef03a739
Andres Arellano <andres.arellano@gmail.com>
2013-10-22 20:08:25 -0400

    aarellano: adds osx.pp

---
+++ modules/people/manifests/aarellano/osx.pp
@@ -0,0 +1,25 @@
+class people::aarellano::osx {
+
+  osx::recovery_message { 'If this Mac is found, please reach me at andres.arellano@gmail.com': }
+
+  include osx::global::disable_key_press_and_hold
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::global::key_repeat_delay
+  include osx::global::key_repeat_rate
+  include osx::global::disable_autocorrect
+
+  include osx::dock::autohide
+  include osx::dock::clear_dock
+  include osx::dock::dim_hidden_apps
+
+  include osx::finder::show_external_hard_drives_on_desktop
+  include osx::finder::show_hard_drives_on_desktop
+  include osx::finder::show_mounted_servers_on_desktop
+  include osx::finder::show_removable_media_on_desktop
+  include osx::finder::unhide_library
+
+  include osx::no_network_dsstores
+
+}
