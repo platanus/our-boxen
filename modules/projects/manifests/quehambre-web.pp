bc91652ed20c103738006430df41165d3e95a793
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-12-06 11:15:48 -0300

    Change repo url for quehambre web

--- modules/projects/manifests/quehambre-web.pp
+++ modules/projects/manifests/quehambre-web.pp
@@ -31,7 +31,7 @@ class projects::quehambre-web {
 
   php::project { 'quehambre-web':
     dir           => "${boxen::config::srcdir}/quehambre/web",
-    source        => 'git@github.com:platanus-repos/quehambre.git',
+    source        => 'git@github.com:platanus/quehambre.git',
     nginx         => "projects/quehambre/nginx.conf.erb",
     php           => '5.3.23',
   }
