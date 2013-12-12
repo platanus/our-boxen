0d02cd5a0762baea11a4afc3270c2f572a51d895
Ignacio Baixas <iobaixas@gmail.com>
2013-12-12 20:08:06 -0300

    ibaixas: setups initial user config

--- modules/people/manifests/iobaixas.pp
+++ modules/people/manifests/iobaixas.pp
@@ -1,15 +1,50 @@
 class people::iobaixas {
 
-  include dropbox
+	include virtualbox
+	include zsh
+	include clipmenu
 
-  $home     = "/Users/${::luser}"
-  $my       = "${home}/my"
-  $dotfiles = "${my}/dotfiles"
+	# Osx config
+  	# include people::iobaixas::osx
 
-  #repository { $dotfiles:
-  #  source  => 'iobaixas/dotfiles',
-  #  require => File[$my]
-  #}
+	sublime_text_2::package { 'Package Control':
+  		source => 'wbond/sublime_package_control'
+  	}
 
-  #include projects::all
+	sublime_text_2::package { 'Git':
+  		source => 'kemayo/sublime-text-2-git'
+	}
+
+	sublime_text_2::package { 'GitGutter':
+  		source => 'jisaacks/GitGutter'
+	}
+
+	sublime_text_2::package { 'Puppet':
+		source => 'russCloak/SublimePuppet'
+	}
+
+	$home = "/Users/${::boxen_user}"
+	$dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+
+	repository { $dotfiles_dir:
+		source => "${::github_login}/dotfiles"
+	}
+
+	file { "${home}/.zshrc":
+		ensure  => link,
+		target  => "${dotfiles_dir}/.zshrc",
+		require => Repository[$dotfiles_dir]
+	}
+
+	file { "${home}/.gitconfig":
+		ensure => link,
+		target => "${dotfiles_dir}/.gitconfig",
+		require => Repository[$dotfiles_dir]
+	}
+
+	file { "${home}/.aliases":
+		ensure => link,
+		target => "${dotfiles_dir}/.aliases",
+		require => Repository[$dotfiles_dir]
+	}
 }
