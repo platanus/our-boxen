861390fa50c571e0af00b356d4fbdc9306b2443d
Jaime Bunzli <jpbunzli@gmail.com>
2013-12-18 10:59:19 -0300

    jaime adds chromedriver

--- modules/people/manifests/bunzli.pp
+++ modules/people/manifests/bunzli.pp
@@ -4,14 +4,5 @@ class people::bunzli {
   include postgresql
   include dropbox
 
-  $home     = "/Users/${::luser}"
-  $my       = "${home}/my"
-  $dotfiles = "${my}/dotfiles"
-
-  #repository { $dotfiles:
-  #  source  => 'bunzli/dotfiles',
-  #  require => File[$my]
-  #}
-
-  #include projects::all
+  package { 'chromedriver': }  
 }
