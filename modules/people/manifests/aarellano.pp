04614cd964506f61f7c126bf60ef8510be9783ad
Andres Arellano <andres.arellano@gmail.com>
2014-01-06 20:54:21 -0500

    aarellano: run different code based on the hostname

--- modules/people/manifests/aarellano.pp
+++ modules/people/manifests/aarellano.pp
@@ -1,90 +1,102 @@
 class people::aarellano {
 
-  include textual
-  include zsh
-  include tmux
-  include better_touch_tools
-  include imagemagick
-  include iterm2::stable
-  include clipmenu
-  include vlc
-  include caffeine
+  case $::hostname {
+    'nomade': {
+      notify { "I'm a Macbook Air": }
+    }
 
-  # Osx config
-  include people::aarellano::osx
+    'lacruz': {
+      notify { "I'm an iMac": }
+    }
 
-  # Yeoman tools
-  nodejs::module { 'yo for v0.10.3':
-    module  => 'yo',
-    node_version => 'v0.10.3',
-  }
+    'default': {
+      include textual
+      include zsh
+      include tmux
+      include better_touch_tools
+      include imagemagick
+      include iterm2::stable
+      include clipmenu
+      include vlc
+      include caffeine
 
-  sublime_text_2::package { 'Package Control':
-    source => 'wbond/sublime_package_control'
-  }
+      # Osx config
+      include people::aarellano::osx
 
-  sublime_text_2::package { 'Git':
-    source => 'kemayo/sublime-text-2-git'
-  }
+      # Yeoman tools
+      nodejs::module { 'yo for v0.10.3':
+        module  => 'yo',
+        node_version => 'v0.10.3',
+      }
 
-  sublime_text_2::package { 'GitGutter':
-    source => 'jisaacks/GitGutter'
-  }
+      sublime_text_2::package { 'Package Control':
+        source => 'wbond/sublime_package_control'
+      }
 
-  sublime_text_2::package { 'Puppet':
-    source => 'russCloak/SublimePuppet'
-  }
+      sublime_text_2::package { 'Git':
+        source => 'kemayo/sublime-text-2-git'
+      }
 
-  sublime_text_2::package { 'Ruby Tests':
-    source => 'maltize/sublime-text-2-ruby-tests'
-  }
+      sublime_text_2::package { 'GitGutter':
+        source => 'jisaacks/GitGutter'
+      }
 
-  $home = "/Users/${::boxen_user}"
-  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+      sublime_text_2::package { 'Puppet':
+        source => 'russCloak/SublimePuppet'
+      }
 
-  repository { $dotfiles_dir:
-    source => "${::github_login}/dotfiles"
-  }
+      sublime_text_2::package { 'Ruby Tests':
+        source => 'maltize/sublime-text-2-ruby-tests'
+      }
 
-  file { "${home}/.zshrc":
-    ensure  => link,
-    target  => "${dotfiles_dir}/.zshrc",
-    require => Repository[$dotfiles_dir]
-  }
+      $home = "/Users/${::boxen_user}"
+      $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
 
-  file { "${home}/.tmux.conf":
-    ensure  => link,
-    target  => "${dotfiles_dir}/.tmux.conf",
-    require => Repository[$dotfiles_dir]
-  }
+      repository { $dotfiles_dir:
+        source => "${::github_login}/dotfiles"
+      }
 
-  file { "${home}/.gitconfig":
-    ensure => link,
-    target => "${dotfiles_dir}/.gitconfig",
-    require => Repository[$dotfiles_dir]
-  }
+      file { "${home}/.zshrc":
+        ensure  => link,
+        target  => "${dotfiles_dir}/.zshrc",
+        require => Repository[$dotfiles_dir]
+      }
 
-  file { "${home}/.aliases":
-    ensure => link,
-    target => "${dotfiles_dir}/.aliases",
-    require => Repository[$dotfiles_dir]
-  }
+      file { "${home}/.tmux.conf":
+        ensure  => link,
+        target  => "${dotfiles_dir}/.tmux.conf",
+        require => Repository[$dotfiles_dir]
+      }
 
-  file { "${home}/.tmux/rails_env":
-    ensure => link,
-    target => "${dotfiles_dir}/.tmux/rails_env",
-    require => Repository[$dotfiles_dir]
-  }
+      file { "${home}/.gitconfig":
+        ensure => link,
+        target => "${dotfiles_dir}/.gitconfig",
+        require => Repository[$dotfiles_dir]
+      }
 
-  file { "${home}/.pythonrc":
-    ensure => link,
-    target => "${dotfiles_dir}/.pythonrc",
-    require => Repository[$dotfiles_dir]
-  }
+      file { "${home}/.aliases":
+        ensure => link,
+        target => "${dotfiles_dir}/.aliases",
+        require => Repository[$dotfiles_dir]
+      }
 
-  file { "${home}/.railsrc":
-    ensure => link,
-    target => "${dotfiles_dir}/.railsrc",
-    require => Repository[$dotfiles_dir]
+      file { "${home}/.tmux/rails_env":
+        ensure => link,
+        target => "${dotfiles_dir}/.tmux/rails_env",
+        require => Repository[$dotfiles_dir]
+      }
+
+      file { "${home}/.pythonrc":
+        ensure => link,
+        target => "${dotfiles_dir}/.pythonrc",
+        require => Repository[$dotfiles_dir]
+      }
+
+      file { "${home}/.railsrc":
+        ensure => link,
+        target => "${dotfiles_dir}/.railsrc",
+        require => Repository[$dotfiles_dir]
+      }
+    }
   }
 }
