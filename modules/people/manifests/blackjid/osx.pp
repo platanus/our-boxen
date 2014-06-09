84f74bb36ce418b413c7eb8159fb03542338e4bd
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-06-09 10:56:24 -0400

    chore(blackjid): Update osx settings

--- modules/people/manifests/blackjid/osx.pp
+++ modules/people/manifests/blackjid/osx.pp
@@ -7,13 +7,20 @@ class people::blackjid::osx {
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::global::key_repeat_delay
-  include osx::global::key_repeat_rate
+  # include osx::global::key_repeat_rate
+  class { 'osx::global::key_repeat_rate':
+    rate => 2
+  }
+  include osx::global::tap_to_click
+  include osx::global::natural_mouse_scrolling
 
   include osx::dock::autohide
   include osx::dock::clear_dock
   include osx::dock::dim_hidden_apps
 
   include osx::finder::show_removable_media_on_desktop
+  include osx::finder::show_external_hard_drives_on_desktop
 
   include osx::no_network_dsstores
+
 }
