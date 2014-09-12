ec2578a7c4d0c7aedd0800a6d9f358f4ed785564
Emilio Blanco <emilioeduardob@gmail.com>
2014-09-12 09:30:30 -0400

    Setup personal manifest

--- modules/people/manifests/emilioeduardob.pp
+++ modules/people/manifests/emilioeduardob.pp
@@ -1,21 +1,6 @@
 class people::emilioeduardob {
-  include textual
   include imagemagick
   include dropbox
-
-  include sublime_text_2
-  sublime_text_2::package { 'EditorConfig':
-    source => 'sindresorhus/editorconfig-sublime'
-  }
-
-  $home     = "/Users/${::luser}"
-  $my       = "${home}/my"
-  $dotfiles = "${my}/dotfiles"
-
-  #repository { $dotfiles:
-  #  source  => 'bunzli/dotfiles',
-  #  require => File[$my]
-  #}
-
-  #include projects::all
+  include zsh
+  include iterm2::stable
 }
