35c29c8eccf51288716533c8dc0207d17e71b5a6
Juan Ignacio Donoso <jidonoso@gmail.com>
2015-05-29 18:44:30 -0300

    chore(android): update android stack

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,7 @@ node default {
   include git
   include hub
   include nginx
+  include brewcask
 
   # fail if FDE is not enabled
   #if $::root_encrypted == 'no' {
--- modules/stacks/manifests/android.pp
+++ modules/stacks/manifests/android.pp
@@ -2,16 +2,17 @@ class stacks::android {
   include java
   include android::sdk
   include android::platform_tools
-  # include android::studio
-  class { 'android::studio':
-    release => '0.8.0',
-    version => '135.1245622',
-  }
 
-  android::build_tools { '20.0.0': }
+  android::build_tools { '21.0.0': }
 
   # Install KitKat 4.4
   android::version { '20':
-    options => ['platform', 'system_image']
+    options => ['platform']
   }
+
+  android::version { '21':
+    options => ['platform']
+  }
+
+  package { 'android-studio': provider => 'brewcask' }
 }
