352f84f05952a829ca84f7f980057cc2f61ef8b9
Jaime Bünzli <jpbunzli@gmail.com>
2015-10-29 17:16:24 -0300

    chore(bunzli): im bunzli

--- modules/people/manifests/bunzli.pp
+++ modules/people/manifests/bunzli.pp
@@ -1,31 +1,7 @@
 class people::bunzli {
-
   npm_module { 'jshint for all nodes':
     module        => 'jshint',
     node_version  => '*',
     version       => '~> 2.8.0'
   }
-
-  # Sublime Text
-  sublime_text::package { 'Package Control':
-    source => 'wbond/sublime_package_control'
-  }
-
-  $sublime_settings = "${boxen::config::srcdir}/sublime-settings"
-  repository { 'Sublime Text Settings':
-    source  => 'bunzli/sublime-settings',
-    path    => $sublime_settings
-  }
-
-  file { "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3/Packages/User":
-    ensure  => link,
-    force   => true,
-    target  => $sublime_settings,
-    require => Repository['Sublime Text Settings']
-  }
-
-  $home     = "/Users/${::luser}"
-  $my       = "${home}/my"
-  $dotfiles = "${my}/dotfiles"
-
 }
