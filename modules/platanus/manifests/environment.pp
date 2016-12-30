779a8fdbd7b65380ce13837e39de6c567f515954
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-12-30 00:30:22 -0500

    chore(): cleanup

--- modules/platanus/manifests/environment.pp
+++ modules/platanus/manifests/environment.pp
@@ -1,11 +1,5 @@
 class platanus::environment{
 
-  # Services
-  class {'pow':
-    domains => 'pow',
-    ext_domains => 'xip.io'
-  }
-
   # Env
   boxen::env_script { 'platanus':
     content  => template('platanus/env.sh.erb'),
--- modules/stacks/manifests/node.pp
+++ modules/stacks/manifests/node.pp
@@ -13,40 +13,4 @@ class stacks::node {
     node_version  => '*',
     version       => '~> 3.8.6'
   }
-
-  npm_module { 'gulp for all nodes':
-    module        => 'gulp',
-    node_version  => '*',
-    version       => '~> 3.9.1'
-  }
-
-  npm_module { 'grunt-cli for all nodes':
-    module        => 'grunt-cli',
-    node_version  => '*',
-    version       => '~> 1.2.0'
-  }
-
-  npm_module { 'ionic for all nodes':
-    module        => 'ionic',
-    node_version  => '*',
-    version       => '~> 1.7.0'
-  }
-
-  npm_module { 'yo for all nodes':
-    module        => 'yo',
-    node_version  => '*',
-    version       => '~> 1.7'
-  }
-
-  npm_module { 'generator-platanus-ionic for all nodes':
-    module        => 'generator-platanus-ionic',
-    node_version  => '*',
-    version       => '~> 0.1.0'
-  }
-
-  npm_module { 'karma-cli for all nodes':
-    module        => 'karma-cli',
-    node_version  => '*',
-    version       => '~> 0.1.2'
-  }
 }
